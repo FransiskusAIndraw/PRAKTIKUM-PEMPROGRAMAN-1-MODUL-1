2541db8ad837977fac56541466ce35d4eb9769a4
FransiskusAIndraw <115336850+FransiskusAIndraw@users.noreply.github.com>
2022-10-15 21:35:40 +0800

    Delete PRAK002-2210817210032-FransiskusAssisiIndraWijaya.c

--- PRAK002-2210817210032-FransiskusAssisiIndraWijaya.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-int main(){
-    printf("Selamat pagi, Fransiskus Assisi Indra Wijaya\n");
-    printf("Selamat siang, Fransiskus Assisi Indra Wijaya\n");
-    printf("Selamat malam, Fransiskus Assisi Indra Wijaya\n");
-   return 0;}
